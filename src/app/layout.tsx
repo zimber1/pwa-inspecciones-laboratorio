258959064aff8e14d6dc4a47c4ec1c0078125be6
zimber1 <claudiaespindolalopez0@gmail.com>
2026-09-09 15:01:04 -0600

    feat: agregar manifest de la aplicacion

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,9 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Inspecciones de laboratorio",
-  description: "Proyecto base de Aplicaciones Web Progresivas"
+  description: "Proyecto base de Aplicaciones Web Progresivas",
+  manifest: "/manifest.webmanifest"
+
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
